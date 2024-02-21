76d6690d449b82abbae894432a2d8cc71c582412
csae8092 <peter.andorfer@oeaw.ac.at>
2024-02-21 12:26:08 +0100

    higher zoom?

--- make_tiles.sh
+++ make_tiles.sh
@@ -4,4 +4,4 @@ wget https://oeawcloud.oeaw.ac.at/index.php/s/g2nmzA5jHQex2m8/download/map_01.ti
 rm -rf ./html/map_01
 mkdir -p ./html/map_01
 
-gdal2tiles.py map_01.tif ./html/map_01 -z '2-7'
+gdal2tiles.py map_01.tif ./html/map_01 -z '2-12'
