0f1ae9d5c659076f48f0263bf950dfdc12b772e1
csae8092 <peter.andorfer@oeaw.ac.at>
2024-02-21 14:41:19 +0100

    lets try 9

--- make_tiles.sh
+++ make_tiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://oeawcloud.oeaw.ac.at/index.php/s/g2nmzA5jHQex2m8/download/map_01.tif
+# wget https://oeawcloud.oeaw.ac.at/index.php/s/g2nmzA5jHQex2m8/download/map_01.tif
 
 rm -rf ./html/map_01
 mkdir -p ./html/map_01
 
-gdal2tiles.py map_01.tif ./html/map_01 -z '2-12'
+gdal2tiles.py map_01.tif ./html/map_01 -z '2-9'
